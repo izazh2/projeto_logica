4c43af29cd4d5aaad04122139155e521d7948c7e
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-06-13 13:36:04 -0300

    Várias alterações no js/script_questao06.js

--- js/script_questao06.js
+++ js/script_questao06.js
@@ -8,12 +8,23 @@ formDados.addEventListener('submit', (evt) => {
 
     const form_num = new FormData(formDados)
 
+    let nome = form_num.get('nome')
     let num1 =  parseFloat(form_num.get('num1'))
     let num2 =  parseFloat(form_num.get('num2'))
 
-    let litros = parseFloat(num1 / num2) / parseFloat(3)
-    let total = parseFloat( litros * num3 )
+    console.log(nome, num1, num2)
 
-  
+    let imc = parseFloat(num1) / parseFloat(num2 * num2)
 
+    if (imc < 20) {
+        divResultado.innerHTML = `<p>${nome}, seu IMC é ${imc.toFixed(2)} e você está abaixo do peso.</p>`
+    } else if ((imc >= 20) && (imc < 25)) {
+        divResultado.innerHTML = `<p>${nome}, seu IMC é ${imc.toFixed(2)} e você está com peso normal.</p>`
+    } else if ((imc >= 25) && (imc < 30)) {
+        divResultado.innerHTML = `<p>${nome}, seu IMC é ${imc.toFixed(2)} e você está acima do peso.</p>`
+    }else if ((imc >= 30) && (imc < 35)) {
+        divResultado.innerHTML = `<p>${nome}, seu IMC é ${imc.toFixed(2)} e você está com obesidade grau 1.</p>`
+    } else if ((imc >= 35) && (imc < 40)) {
+        divResultado.innerHTML = `<p>${nome}, seu IMC é ${imc.toFixed(2)} e você está com obesidade grau 2.</p>`
+    }
 })
